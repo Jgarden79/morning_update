4d719572788d0b3d95bc794995c1ccd8e391a08c
Jgarden79 <jgarden79@gmail.com>
2021-03-01 07:51:26 -0800

    equit db stage 1

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#eikon
+eikon_key = 'b4229eae2c644ec8a0aab0bb0fb83b13a38eb2c7'
+username_ds = 'JGarden1@lidoadvisors.com'
+pw_ds = 'Welcome2'
+
